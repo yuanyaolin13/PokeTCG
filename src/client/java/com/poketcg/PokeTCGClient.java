899a94fea467ecc8612e7fdcdddf90f195c7e68d
Yuanyao Lin <71469791+yuanyaolin13@users.noreply.github.com>
2024-08-25 17:04:41 -0700

    Add files via upload

---
+++ src/client/java/com/poketcg/PokeTCGClient.java
@@ -0,0 +1,10 @@
+package com.poketcg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PokeTCGClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
